5a4be1cc693f1938de694a7894cc6ada66b8b96c
de_Mora <miguel.uni.demora@gmail.com>
2020-02-17 11:31:33 +0100

    changed routes and user model

--- madriz-de-cine/app.js
+++ madriz-de-cine/app.js
@@ -82,10 +82,10 @@ app.use(flash());
 require('./passport')(app);
     
 
-const index = require('./routes/index');
+const index = require('./routes/index.routes');
 app.use('/', index);
 
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth.routes');
 app.use('/auth', authRoutes);
       
 
