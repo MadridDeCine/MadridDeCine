0b3bcd439924cc26999213fc757ee1e07ddf4b13
Carolina Guzman <carolinaguzman76@gmail.com>
2020-02-19 17:04:55 +0100

    iconos camaras en mapa y titulo

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -8,30 +8,52 @@ let initialCoords = {
 // Funcion que incia el mapa
 
 function initMap() {
-    let mapOptions = { center: initialCoords, zoom: 15 }
+    let mapOptions = { center: initialCoords, zoom: 14 }
     myMap = new google.maps.Map(document.querySelector('#myMap'), mapOptions)
-    getFilms()
+    getFilms(myMap)
 }
 
 
-function getFilms() {
+function getFilms(myMap) {
 
     axios.get("/film/api")
         .then(response => {
             const allFilms = response.data
             console.log('el array de peliculas es:', allFilms)
-            placeFilmsInMap(allFilms)
+            placeFilmsInMap(allFilms, myMap)
         })
         .catch(error => console.log(error))
 }
 
 
-function placeFilmsInMap(films) {
-    console.log()
+// function placeFilmsInMap(films) {
+//     films.forEach(oneFilm => {
+//         const center = { lat: oneFilm.coords.lat, lng: oneFilm.coords.lng }
+//         new google.maps.Marker({ position: center, map: myMap, title: oneFilm.title })
+//     })
+// }
 
-    films.forEach(oneFilm => {
-        const center = { lat: oneFilm.coords.lat, lng: oneFilm.coords.lng }
-        new google.maps.Marker({ position: center, map: myMap, title: oneFilm.title })
-    })
-}
+// Fuente imagentes
+let iconBase = 'https://maps.google.com/mapfiles/kml/shapes/'
+
+function placeFilmsInMap(films, myMap) {
+    var activeInfoWindow
+        films.forEach(oneFilm => {
+            console.log(oneFilm.title)
+            const center = { lat: oneFilm.coords.lat, lng: oneFilm.coords.lng }
+            let marker = new google.maps.Marker({ position: center, map: myMap, icon: iconBase + 'movies.png' })
+            let infowindow = new google.maps.InfoWindow({content: oneFilm.title, closeOnMapClick:true, shadow:true})
+            marker.addListener('click', () => {
+                if (activeInfoWindow) {
+                  activeInfoWindow.close();
+                }
+                infowindow.open(myMap, marker);
+                activeInfoWindow = infowindow;
+              })
+
+        })
+        //console.log(title)
+    }
+
+ 
 
