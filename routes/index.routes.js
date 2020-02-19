745e1c497e478543ad89ec42eea1974f90acb0e2
Carolina Guzman <carolinaguzman76@gmail.com>
2020-02-19 11:26:31 +0100

    visualiza el mapa pero muestra api key

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const router  = express.Router();
 
 /* GET home page */
+const secret = {SECRET:process.env.SECRET}
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index',secret);
 });
 
 module.exports = router;
