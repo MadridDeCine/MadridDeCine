2409edd5c0d31db725bffb037f83843e7cd6d297
Carolina Guzman <carolinaguzman76@gmail.com>
2020-02-19 18:58:10 +0100

    ir a detalles desde incono del mapa

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -3,7 +3,7 @@
 let initialCoords = {
     lat: 40.4165000,
     lng: -3.7025600
-},myMap
+}, myMap
 
 // Funcion que incia el mapa
 
@@ -25,35 +25,38 @@ function getFilms(myMap) {
         .catch(error => console.log(error))
 }
 
-
-// function placeFilmsInMap(films) {
-//     films.forEach(oneFilm => {
-//         const center = { lat: oneFilm.coords.lat, lng: oneFilm.coords.lng }
-//         new google.maps.Marker({ position: center, map: myMap, title: oneFilm.title })
-//     })
-// }
-
 // Fuente imagentes
 let iconBase = 'https://maps.google.com/mapfiles/kml/shapes/'
 
 function placeFilmsInMap(films, myMap) {
     var activeInfoWindow
-        films.forEach(oneFilm => {
-            console.log(oneFilm.title)
-            const center = { lat: oneFilm.coords.lat, lng: oneFilm.coords.lng }
-            let marker = new google.maps.Marker({ position: center, map: myMap, icon: iconBase + 'movies.png' })
-            let infowindow = new google.maps.InfoWindow({content: oneFilm.title, closeOnMapClick:true, shadow:true})
-            marker.addListener('click', () => {
-                if (activeInfoWindow) {
-                  activeInfoWindow.close();
-                }
-                infowindow.open(myMap, marker);
-                activeInfoWindow = infowindow;
-              })
+    films.forEach(oneFilm => {
+        console.log(oneFilm.title)
+        const center = { lat: oneFilm.coords.lat, lng: oneFilm.coords.lng }
+        let marker = new google.maps.Marker({ position: center, map: myMap, icon: iconBase + 'movies.png' })
+        let infowindow = new google.maps.InfoWindow({ content: oneFilm.title, closeOnMapClick: true, shadow: true })
+        marker.addListener('click', () => {
+            // if (activeInfoWindow) {
+            //     activeInfoWindow.close();
+            // }
+            // infowindow.open(myMap, marker);
+            // activeInfoWindow = infowindow;
+            window.location=`/film/${oneFilm._id}`
 
+                // axios.get('/login', {username: username, password: password})
+                //     .then(function (response) {
+                //         if (response.data.redirect == '/') {
+                //             window.location = "/index"
+                //         } else if (response.data.redirect == '/login'){
+                //             window.location = "/login"
+                //         }
+                //     })
+                //     .catch(function(error) {
+                //         window.location = "/login"
+                //     })
         })
-        //console.log(title)
-    }
+    })
+}
 
- 
+
 
